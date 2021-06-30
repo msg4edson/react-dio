3a021ebc36886362a25cab707f06bda42b9c8049
msg4edson <msg.edson@gmail.com>
2021-06-29 23:11:30 -0300

    Create first component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import Item from "./components/Item";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Minha primeira aplicação com React</h1>
+      <p>Eu sou um paragrafo</p>
+      <ul>
+        <Item>Item 1</Item>
+        <Item>Item 2</Item>
+        <Item>Item 3</Item>
+      </ul>
+    </>
   );
-}
+};
 
 export default App;
